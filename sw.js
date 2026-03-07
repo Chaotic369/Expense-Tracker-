1a08c8f5b5922025e75bf6d0f1bac77250068203
Chaotic369 <chadfor19@gmail.com>
2026-03-07 13:43:47 +0530

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'expense-tracker-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;800&display=swap'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  // Only cache GET requests (don't interfere with Firebase interactions)
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
